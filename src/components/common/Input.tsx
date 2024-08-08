968525338f31958640be03a945487b18e0289afc
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-08-08 17:01:03 +0900

    Design: Input 스타일 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,41 +1,60 @@
 import styled from "@emotion/styled";
-import React, { ReactNode } from "react";
+import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 interface InputProps {}
 
 const Input: React.FC<InputProps> = ({}) => {
   return (
-    <StyledInput>
-      <input
-        onChange={(e) => console.log(e.target.value)}
-        placeholder="휴대폰 번호, 이메일 주소 또는 사용자 아이디"
+    <ThemeProvider theme={customTheme()}>
+      <TextField
+        id="filled-basic"
+        label="휴대폰 번호, 이메일 주소 또는 사용자 아이디"
+        variant="filled"
       />
-    </StyledInput>
+    </ThemeProvider>
   );
 };
 
-const StyledInput = styled.div`
-  height: 56px;
-  width: 300px;
-
-  input {
-    width: 100%;
-    height: 100%;
-    background-color: transparent;
-    border: 1px solid rgb(51, 54, 57);
-    border-radius: 4px;
-    color: rgb(113, 118, 123);
-    font-size: 17px;
-    padding-left: 12px;
-
-    &:focus, &:hover {
-      outline: none;
-      border: 1px solid rgb(51, 54, 57);
-    }
-
-    &::placeholder {
-    }
-  }
-`;
+const customTheme = () =>
+  createTheme({
+    components: {
+      MuiFormLabel: {
+        styleOverrides: {
+          root: {
+            "&": {
+              fontSize: "17px",
+              color: "rgb(51, 54, 57)",
+            },
+          },
+        },
+      },
+      MuiFilledInput: {
+        styleOverrides: {
+          root: {
+            "&": {
+              height: "56px",
+              width: "300px",
+              border: "2px solid rgb(51, 54, 57)",
+              borderRadius: "4px",
+              color: "#fff",
+            },
+            "&::before, &::after, &:hover:not(.Mui-disabled, .Mui-error):before":
+              {
+                border: "none",
+              },
+            "&.Mui-focused": {
+              height: "56px",
+              width: "100%",
+              border: "2px solid rgb(29, 155, 240)",
+            },
+            "&.Mui-focused:after": {
+              border: "none",
+            },
+          },
+        },
+      },
+    },
+  });
 
 export default Input;
